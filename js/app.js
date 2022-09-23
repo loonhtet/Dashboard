da9252dae32b05bec838f9dd3e6430a312b24ab6
loonhtet <gurdamanson@gmail.com>
2022-09-23 16:21:19 +0630

    23/9/22

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 const searchInput = document.getElementById("searchinput");
 const searchBtn = document.getElementById("searchbtn");
 
+const disItems = document.querySelectorAll("#dis");
+console.log(disItems);
+
 searchBtn.addEventListener("click", searchfn);
 
 console.log(searchInput.clientWidth);
@@ -8,7 +11,15 @@ console.log(searchInput.clientWidth);
 function searchfn() {
   if (searchInput.classList.contains("active")) {
     searchInput.classList.remove("active");
+
+    disItems.forEach(function (e) {
+      e.classList.remove("d-none");
+    });
   } else {
     searchInput.classList.add("active");
+
+    disItems.forEach(function (e) {
+      e.classList.add("d-none");
+    });
   }
 }
