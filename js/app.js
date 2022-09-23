cddbf6b86584680cc29bca2cbdb382cd3c3e368b
loonhtet <gurdamanson@gmail.com>
2022-09-23 17:48:48 +0630

    23/9/22

--- js/app.js
+++ js/app.js
@@ -10,16 +10,30 @@ console.log(searchInput.clientWidth);
 
 function searchfn() {
   if (searchInput.classList.contains("active")) {
+    // Close search box
+
     searchInput.classList.remove("active");
 
     disItems.forEach(function (e) {
       e.classList.remove("d-none");
     });
+    searchBtn.style.color = "var(--bs-dark)";
+
+    searchBtn.classList.remove("ri-close-line");
+    searchBtn.classList.add("ri-search-line");
   } else {
+    // Open search box
+
     searchInput.classList.add("active");
 
     disItems.forEach(function (e) {
       e.classList.add("d-none");
     });
+
+    searchBtn.classList.remove("ri-search-line");
+    searchBtn.classList.add("ri-close-line");
+    searchBtn.style.color = "var(--bs-danger)";
+
+    searchInput.focus();
   }
 }
